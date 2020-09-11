07e0abc174909eceebf787f765efe453c55f0383
Girmay <ink423103@yahoo.com>
2020-09-11 14:13:57 +0200

    todo component added

---
+++ TodoList/angular-todolist/src/app/components/todo/todo.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+
+import { Todo } from 'src/app/models/Todo';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+  @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
+
+  constructor(private todoService:TodoService) { }
+
+  ngOnInit() {
+  }
+
+  // Set Dynamic Classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+
+    return classes;
+  }
+
+  onToggle(todo) {
+    // Toggle in UI
+    todo.completed = !todo.completed;
+    // Toggle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo));
+  }
+
+  onDelete(todo) {
+    this.deleteTodo.emit(todo);
+  }
+
+}
