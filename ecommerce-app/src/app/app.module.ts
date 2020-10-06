dfd0a9d7c4ac9bf394d4348abd0024d31c46cbfd
Girmay <ink423103@yahoo.com>
2020-10-06 22:36:38 +0200

    app module added

--- ecommerce-app/src/app/app.module.ts
+++ ecommerce-app/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CartComponent } from './components/cart/cart.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
+import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductComponent } from './components/product/product.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ThankyouComponent } from './components/thankyou/thankyou.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +18,12 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
     HomeComponent,
     HeaderComponent,
     CartComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    LoginComponent,
+    FooterComponent,
+    ProductComponent,
+    RegisterComponent,
+    ThankyouComponent
   ],
   imports: [
     BrowserModule
